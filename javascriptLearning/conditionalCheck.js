c9466442d2aff8e585bc268c5be5014fbe95e0e2
ayush <ayush.ranjan921@gmail.com>
2026-03-20 18:11:52 +0530

    Learned few loops defining

--- javascriptLearning/conditionalCheck.js
+++ javascriptLearning/conditionalCheck.js
@@ -49,3 +49,48 @@ function tryingSwitchCase(conditionForSwitchCase){
 };
 
 tryingSwitchCase(2);
+
+//Checking multiple if conditions
+
+let a = 10;
+
+if(a == 10){
+    console.log("The value of a is " +a);
+}
+if(a == 11) {
+    console.log("The value of a is 11");
+} 
+if(a == 12){
+    console.log("The value of a is 12");
+} else {
+    console.log("The value of a is not between 10 to 12");
+}
+
+/*
+From the above multiple if cases we can see that the output have:
+The value of a is 10
+The value of a is not between 10 to 12
+That means it is checking all the condition of if,
+*WE CANNOT USE THE BREAK STATEMENT DIRECTLY TO THE IF CASE IF IT IS NOT IN THE LOOP*
+*/
+
+//To solve the above if-if problem we use if else condition in JS
+
+//Let's check function with return type
+
+function addTwoNumberAndReturn(a, b){
+return a+b;
+};
+
+let returnedNumber = addTwoNumberAndReturn(3,"a");
+
+console.log("Sum of given two number is: " +returnedNumber)
+
+
+function divideTwoNumberAndReturn(a, b){
+return a/b;
+};
+
+let returnedNumberOfDivide = divideTwoNumberAndReturn("22","7");
+
+console.log("Sum of given two number is: " +returnedNumberOfDivide)
---
+++ javascriptLearning/loops.js
@@ -0,0 +1,31 @@
+// for(let a =1; a<=10; a++){
+// console.log("Checking loop in js")
+// };
+
+
+function writingTablesForProvidedNumber(tableNumber, tillCount){
+
+    for(let a = 1; a<=tillCount; a++){
+        console.log(tableNumber +" * " + a + " = "+ tableNumber*a);
+    }
+};
+
+//writingTablesForProvidedNumber(2,20);
+
+function findInterestPercentage(principalAmount, amountPaidAsInterest){
+
+    let percentage = (amountPaidAsInterest * 100) / principalAmount * 12;
+
+    return percentage;
+};
+
+console.log(findInterestPercentage(15037, 38));
+
+function findInterestAmount(principalAmountNew, interestPercentage){
+
+    let interestAmountPerAnnum = (principalAmountNew * (interestPercentage / 100));
+
+    return interestAmountPerAnnum;
+};
+
+console.log(findInterestAmount(15037, 3));
